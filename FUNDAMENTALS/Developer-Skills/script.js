9901ba2ab49fb08fed18780ab9c3e4e0a5fb161b
iamdhrey <damilare.nelson@outlook.com>
2023-06-09 09:05:11 -0600

    Debugging snippet added.

--- FUNDAMENTALS/Developer-Skills/script.js
+++ FUNDAMENTALS/Developer-Skills/script.js
@@ -55,3 +55,74 @@ FIX
 meaning wright a humman readable code before you code.
 5. develop curiousity on how things work.
 */
+
+//                                                  DEBUGGING.
+/*
+BUG: Is a defect or problem in a computer program. it an unexpected or unintended behaviour in a computer program.
+DEBUGGING: is finding and fixing a problem
+
+PROCESS
+1. Identify the bug with automated test
+2. find the bug with debugger software
+3. correct the bug once found
+4. prevent reoccurence
+*/
+
+// USING A DEBUGGER IN THE BROWSER
+/*
+Go to sources > script and look for the code script snf pin down the point of execution.(breakpoint) meaning when the code runs, it will stop at that execution point.
+
+*/
+
+//EXAMPLE OF FINDING A BUG.
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     value: Number(prompt('Degress Celsius')), // prompt will always produce a string
+//   };
+
+//   console.log(measurement);
+//   console.table(measurement);
+//   //   console.warn(measurement.value);
+//   //   console.error(measurement.value);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// console.log(measureKelvin());
+
+// CODE CHALLENGE
+
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+// const arr = data1.concat(data2);
+
+// const printForcast = function (arr) {
+//   return (arr = `Will print ${arr[0]} in 1 day... ${arr[1]} in 2days ... ${arr[2]} in 3 days `);
+// };
+// console.log(printForcast(arr));
+
+//OR
+
+// const printForcast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str = str + `${arr[i]} in ${i + 1} days ...`;
+//   }
+//   console.log('...' + str);
+// };
+// printForcast(data1);
+
+/* PROBLEM SOLVING FRAMEWORK 
+1. Understand the problem
+i. Array transform to string seperated by 3 dots
+ii. what is the x days? answer : index + 1
+
+2. Breaking up into sub problem
+i. transform array into string
+ii. transform each element to string
+iii. strings needs to contain day (index + 1)
+iv. add ... between element at the start and end of string
+*/
